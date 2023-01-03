556b2dfc614e10f905cbcdb65950be8cbb81f3a6
yejunho10 <hyjcompany30@naver.com>
2023-01-03 12:55:52 +0900

    feat: System Update

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ import {
     GatewayIntentBits,
     Routes,
     Events,
-    AuditLogEvent
+    AuditLogEvent,
+    EmbedBuilder,
+    Collection
 } from "discord.js";
 
 //SETUP MODULES
@@ -12,6 +14,8 @@ dotenv.config();
 
 //VARIABLES
 const TOKEN = process.env.TOKEN;
+const ban_coll = new Collection();
+const unban_coll = new Collection();
 
 ///SETUP CLIENT
 const client = await new Client({
@@ -30,21 +34,45 @@ await client.login(TOKEN).then(() => {
 
 //LISTENERS
 client.on(Events.GuildBanAdd, async (ban) => {
+    if (ban_coll.has(ban.guild.id) && ban_coll.get(ban.guild.id) === ban.user.id) {
+        ban_coll.delete(ban.guild.id);
+        return;
+    }
     console.log(`${ban.user.tag} 님은 ${ban.guild.name} 서버에서 차단 되었습니다.`);
 
-    client.guilds.cache.forEach(guild => guild.members.fetch(ban.user.id).catch(() => {}).then(member => {
-        if (member) {
-            member.ban();
-            member.user.send(`당신은 \`${member.guild.name}\` 서버에서 차단 되었습니다.\n따라서, \`${guild.name}\` 서버에서도 차단 되었습니다.`).catch(() => {});
-        }
-    }));
+    client.guilds.cache.forEach(guild => {
+
+        ban.user.send(`당신은 \`${ban.guild.name}\` 서버에서 차단 되었습니다.\n따라서, \`${guild.name}\` 서버에서도 차단 되었습니다.`).catch(() => {});
+
+        ban_coll.set(guild.id, ban.user.id);
+        guild.bans.create(ban.user.id, {
+            reason: `${ban.user.tag} 님은 ${ban.guild.name} 서버에서 차단 되었습니다.`
+        });
+    });
+
+    const embed = new EmbedBuilder()
+        .setTitle(`<a:attention:1058939472848375819> 연동 차단 | \`${ban.user.tag}\`님 <a:attention:1058939472848375819>`)
+        .setDescription(`\` 🆔 디스코드 아이디:\` ${ban.user}\n\` 🔖 사유:\` 파트너 서버(\`${ban.guild.name}\`)에서 차단 되었습니다.`)
+        .setColor(process.env.RED_EMBED_COLOR);
+
+    if (ban.guild.id !== process.env.MAIN_GUILD_ID) {
+        await (await (await client.guilds.fetch(process.env.MAIN_GUILD_ID)).channels.fetch(process.env.PUNISHMENT_LOG_CHANNEL_ID)).send({embeds: [embed]});
+    }
 });
 
 client.on(Events.GuildBanRemove, async (ban) => {
+    if (unban_coll.has(ban.guild.id) && unban_coll.get(ban.guild.id) === ban.user.id) {
+        unban_coll.delete(ban.guild.id);
+        return;
+    }
     console.log(`${ban.user.tag} 님은 ${ban.guild.name} 서버에서 차단 해제 되었습니다.`);
 
     client.guilds.cache.forEach(guild => {
-        if (guild.bans.cache.has(ban.user.id)) guild.bans.remove(ban.user.id);
-        ban.user.send(`당신은 \`${member.guild.name}\` 서버에서 차단 해제 되었습니다.\n따라서, \`${guild.name}\` 서버에서도 차단 해제 되었습니다.`).catch(() => {});
+        ban.user.send(`당신은 \`${ban.guild.name}\` 서버에서 차단 해제 되었습니다.\n따라서, \`${guild.name}\` 서버에서도 차단 해제 되었습니다.`).catch(() => {});
+
+        if (guild.bans.cache.has(ban.user.id)) {
+            unban_coll.set(guild.id, ban.user.id);
+            guild.bans.remove(ban.user.id);
+        }
     });
 });
